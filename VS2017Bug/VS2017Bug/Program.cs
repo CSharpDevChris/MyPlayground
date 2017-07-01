c3886992888731a36a611834dac7b45b7527172c
Christian Wall <christian.wall@buetema-ag.de>
2017-07-01 12:17:31 +0200

    Add VS2017 Bug Project

---
+++ VS2017Bug/VS2017Bug/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace VS2017Bug
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MyTest.PrintCompileTime();
+      Console.ReadLine();
+    }
+  }
+}
